bf77a82d459e71212c45cb084a7987e86bc1a472
Nachiketa Gargi <nachiketa.gargi@gmail.com>
2017-04-25 20:57:31 -0700

    added REST server

--- scraper.js
+++ scraper.js
@@ -1,6 +1,12 @@
+var restify = require('restify');
+require('dotenv').config();
+
 houseEvents = require("./sources/events.js");
 housePoints = require("./sources/points.js");
 
-modular.exports = function() {
-	return {events: houseEvents, points: housePoints };
-}
+var server = restify.createServer();
+server.get('/events', function(req, res, next) {
+	res.send(houseEvents);
+});
+
+server.listen(process.env.PORT || 8080);
